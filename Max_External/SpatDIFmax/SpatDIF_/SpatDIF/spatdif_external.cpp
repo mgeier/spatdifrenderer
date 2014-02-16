3960404eb47c9ee3d2e48b01e301df83c7e5a2ba
jasch <j45ch4@gmail.com>
2014-02-16 13:41:12 +0100

    some C++ fixes (adding lstdc++.dylib to build copy phase)

--- Max_External/SpatDIFmax/SpatDIF_/SpatDIF/spatdif_external.cpp
+++ Max_External/SpatDIFmax/SpatDIF_/SpatDIF/spatdif_external.cpp
@@ -5,7 +5,8 @@
 
 #include "ext.h"							// standard Max include, always required
 #include "ext_obex.h"						// required for new style Max object
-#include "sdMain.h"
+
+//#include "sdMain.h"
 
 typedef struct _spatdif 
 {
